7dbf048b8ed6196e804991616b9779472a022fbf
Guillaume Communie <gui-co@posteo.net>
2021-06-13 15:32:30 +0200

    Increase contents of read

--- inc/BamRead.h
+++ inc/BamRead.h
@@ -1,14 +1,22 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 class BamRead {
 
 public:
-    BamRead(const std::string &name);
+    BamRead(void);
+    void setName(char *array, size_t length);
+    std::string getName(void) const;
+    void setReadSequence(uint8_t *array, size_t length);
+    void setCigar(uint32_t *array, size_t length);
+    std::vector<uint32_t> getCigar(void) const;
 
 private:
     std::string name;
+    std::vector<uint8_t> read;
+    std::vector<uint32_t> cigar;
 
 };
 
--- src/BamRead.cpp
+++ src/BamRead.cpp
@@ -1,8 +1,27 @@
 #include "BamRead.h"
 
-BamRead::BamRead(const std::string &name)
-    : name(name)
+BamRead::BamRead(void)
 {
     /* Nothing to do */
+}
+
+void BamRead::setName(char *array, size_t length) {
+    name = std::string(array, length);
+}
+
+std::string BamRead::getName(void) const{
+    return name;
+}
+
+void BamRead::setReadSequence(uint8_t *array, size_t length) {
+    read.assign(array, array + length);
+}
+
+void BamRead::setCigar(uint32_t *array, size_t length) {
+    cigar.assign(array, array + length);
+}
+
+std::vector<uint32_t> BamRead::getCigar(void) const {
+    return cigar;
 }
 
