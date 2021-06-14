90ebcc09677ef995ba69ff01975e792caeca1c90
Guillaume Communie <gui-co@posteo.net>
2021-06-14 19:40:07 +0200

    Add options parser in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,43 @@
-#include "BamReader.h"
+#include <unistd.h>
+
+#include <iostream>
+
+void usage(const char *prog) {
+    std::cout << "Usage: " << prog << " -f <fasta_file> -b <bam_file>"
+              << std::endl;
+}
+
 
 int main(int argc,  char** argv) {
-    BamReader reader;
-    reader.setBamFile(argv[1]);
+    int opt;
+    std::string fastaFile;
+    std::string bamFile;
+    while ((opt = getopt(argc, argv, "f:b:")) != -1) {
+        switch (opt) {
+            case 'f':
+                fastaFile = std::string(optarg);
+                break;
+            case 'b':
+                bamFile = std::string(optarg);
+                break;
+            default:
+                usage(argv[0]);
+                return -1;
+        }
+    }
+
+    if (fastaFile.empty()) {
+        std::cout << "[Error]: please provide a fasta file" << std::endl;
+        usage(argv[0]);
+        return -1;
+    }
+
+    if (bamFile.empty()) {
+        std::cout << "[Error]: please provide a bam file" << std::endl;
+        usage(argv[0]);
+        return -1;
+    }
+
+    return 0;
 }
 
