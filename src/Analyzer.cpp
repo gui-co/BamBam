47d1fa0ef1bb3d9915ce53cfa5406bc92e1af986
Guillaume Communie <gui-co@posteo.net>
2021-06-16 19:08:49 +0200

    Extract data from cigar string

--- src/Analyzer.cpp
+++ src/Analyzer.cpp
@@ -39,6 +39,69 @@ void Analyzer::analyze(void) {
                          "found in fasta file" << std::endl;
             continue;
         }
+
+        Analysis *analysis;
+        if (results.find(sequenceName) == results.end()) {
+            results[sequenceName] = new Analysis(sequenceName, refSeq);
+        }
+        analysis = results.find(sequenceName)->second;
+
+        uint8_t flag = read.getFlag();
+        Analysis::StrandPolarity polarity;
+        if (flag & 0x10) {
+            polarity = Analysis::PLUS_STRAND;
+        } else {
+            polarity = Analysis::MINUS_STRAND;
+        }
+        size_t refPos = read.getStartPosition();
+        size_t alnPos = 0;
+        std::string alnSeq = read.getAlignedSequence();
+        std::string cigar = read.getCigar();
+        for (size_t i = 0; i < cigar.size(); i++) {
+            switch (cigar[i]) {
+                case 'M':
+                    if (alnSeq[alnPos] == refSeq[refPos]) {
+                        analysis->addMatch(refPos, polarity);
+                    } else {
+                        analysis->addMismatch(refPos, polarity);
+                    }
+                    refPos++;
+                    alnPos++;
+                    break;
+                case 'I':
+                    if (refPos > 0) {
+                        analysis->addInsertion(refPos - 1, polarity);
+                    }
+                    alnPos++;
+                    break;
+                case 'D':
+                    analysis->addDeletion(refPos, polarity);
+                    refPos++;
+                    break;
+                case 'N':
+                    analysis->addDeletion(refPos, polarity);
+                    refPos++;
+                    break;
+                case 'S':
+                    alnPos++;
+                    break;
+                case 'H':
+                    break;
+                case 'P':
+                    break;
+                case '=':
+                    analysis->addMatch(refPos, polarity);
+                    refPos++;
+                    alnPos++;
+                    break;
+                case 'X':
+                    analysis->addMismatch(refPos, polarity);
+                    refPos++;
+                    alnPos++;
+                    break;
+            }
+        }
+
         read = bamReader->getNextRead();
     }
 }
