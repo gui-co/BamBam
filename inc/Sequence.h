5b73bab367e2fba5139e3be5e60daef44f6788e9
Guillaume Communie <gui-co@posteo.net>
2021-07-14 15:47:49 +0200

    Useless statements

--- inc/Sequence.h
+++ inc/Sequence.h
@@ -10,8 +10,6 @@ class Sequence : public std::list<Transcript> {
 public:
     Sequence(void) = default;
     Sequence(const std::string &sequenceName, const std::string &sequence);
-    Sequence(Sequence &&sequence) = default;
-    Sequence &operator=(Sequence &&rhs) = default;
     Transcript &getTranscript(Transcript::StrandPolarity polarity,
                               size_t position);
     std::string getName(void);
